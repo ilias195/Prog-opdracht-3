28d2f9702d728e88b4713f372885a048a8c08465
ilias195 <iliasafc@outlook.com>
2024-09-12 15:02:18 +0200

    complete opdracht 3

---
+++ op3.js
@@ -0,0 +1,23 @@
+
+let name ="Hendrik de derde";
+let age = 25;
+let power = "magic";
+let lives = 3;
+let weapon = "magic wand";
+let strengh = 150;
+let level = 5999;
+let vehicle = "coach";
+let place = "Kingdom";
+let wanted= true;
+
+console.log (name);
+console.log ( age);
+console.log  (power);
+console.log (lives);
+console.log (weapon);
+console.log  (strengh);
+console.log ( level);
+console.log (vehicle);
+console.log  (place);
+console.log ( wanted);
+    
